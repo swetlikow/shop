34cbe184efb3f37cc6d16681d53ae1795650aab5
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-08-03 15:21:23 +0300

    task1-comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartListComponent } from './cart-list/cart-list.component';
-import { CartListService } from './cart-list/cart-list.service';
 import { FirstComponent } from './first/first.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product/product.component';
-import { ProductsService } from './product/products.service';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        FirstComponent,
-        ProductComponent,
-        ProductListComponent,
-        CartListComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule
-    ],
-    providers: [ProductsService, CartListService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    ProductComponent,
+    ProductListComponent,
+    CartListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  // сервисы уже зарегистрированы, регистрировать их ту нет необходимости
+  // providers: [ProductsService, CartListService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cart-list/cart-list.component.ts
+++ src/app/cart-list/cart-list.component.ts
@@ -3,17 +3,17 @@ import { IProduct } from '../models/product';
 import { CartListService } from './cart-list.service';
 
 @Component({
-    selector: 'app-cart-list',
-    templateUrl: './cart-list.component.html',
-    styleUrls: ['./cart-list.component.css']
+  selector: 'app-cart-list',
+  templateUrl: './cart-list.component.html',
+  styleUrls: ['./cart-list.component.css']
 })
 export class CartListComponent implements OnInit {
 
-    boughtProducts: IProduct[];
-    constructor(private cartListService: CartListService) { }
+  boughtProducts: IProduct[]; // обычно тут хранится еще и количество купленого товара
+  constructor(private cartListService: CartListService) { }
 
-    ngOnInit(): void {
-        this.boughtProducts = this.cartListService.getBoughtProducts();
-    }
+  ngOnInit(): void {
+    this.boughtProducts = this.cartListService.getBoughtProducts();
+  }
 
 }
