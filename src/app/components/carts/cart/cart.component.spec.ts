11d5709e86c54d7827f320d4367fb4e3b4eae19c
swetlikowtrans <swetlikowtrans@gmail.com>
2020-10-10 20:36:26 +0200

    unit add cartComponent spec

--- src/app/components/carts/cart/cart.component.spec.ts
+++ src/app/components/carts/cart/cart.component.spec.ts
@@ -1,25 +1,39 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Product } from '../../products/product/product';
+import { CartComponent } from './cart.component';
 
-// import { CartComponent } from './cart.component';
+describe('CartComponent', () => {
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
+  const product: Product = new Product(1, 'TestName', 5, 10, false);
+  const listItemContent = `Product: ${product.name} Count: ${product.count}`;
 
-// describe('CartComponent', () => {
-//   let component: CartComponent;
-//   let fixture: ComponentFixture<CartComponent>;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CartComponent],
+    }).compileComponents();
+  }));
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ CartComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CartComponent);
+    component = fixture.componentInstance;
+    component.product = product;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CartComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    debugElement = fixture.debugElement.query(By.css('.list-group-item'));
+    htmlElement = debugElement.nativeElement;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create list-item content with product.name and product.count', () => {
+    expect(htmlElement.innerText).toBe(listItemContent);
+  });
+});
